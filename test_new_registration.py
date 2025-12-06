b4e78c5491c6794c7a7d9ecf6fb42741a14db0bd
Jan-Kudela <jochanan.jorgen@gmail.com>
2025-12-06 21:22:18 +0100

    zkrácení zápisu a přidání funkce pro generování náhodného emailu pro test new_registration

--- test_new_registration.py
+++ test_new_registration.py
@@ -1,37 +1,31 @@
 from playwright.sync_api import Page, sync_playwright
 import pytest
+import time
 from test_dumhudby import page, accept_cookies
 
 
+def generate_email():
+    """vrací unikátní email - časové razítko"""
+    return f"test_{int(time.time())}@testmail.cz"
+
+
 def test_new_registration(page: Page):
-    
+    email = generate_email()
     page.goto("https://www.dumhudby.cz/")
     accept_cookies(page)
-    prihlasit_se_button = page.locator("a.user-anchor")
-    prihlasit_se_button.click()
-    nova_reg_button = page.locator("a.btn.btn-big.btn-second.btn-wide.mtop10")
-    nova_reg_button.click()
-    email_fill = page.locator("#email")
-    email_fill.fill("jaann@gmail.com")
-    password = page.locator("#pwd_reg")
-    password.fill("motyl55")
-    password_2 = page.locator("#pwd1")
-    password_2.fill("motyl55")
-    name = page.locator("#name")
-    name.fill("Jan")
-    surname = page.locator("#surname")
-    surname.fill("Novák")
-    phone = page.locator("#phone")
-    phone.fill("+420608888555")
-    street = page.locator("#street")
-    street.fill("Horní 666")
-    city = page.locator("#city")
-    city.fill("České Budějovice")
-    zip = page.locator("#zip")
-    zip.fill("74755")
-    submit_button = page.locator("#content > main > form > div.buttons > input")
-    submit_button.click()
-    page.pause()
-    registration_ok = page.locator("#registrace_ok > span")
+    page.locator("a.user-anchor").click() #prihlasit se button
+    page.locator("a.btn.btn-big.btn-second.btn-wide.mtop10").click()
+     # nová registrace button
+    page.locator("#email").fill(f"{email}") #email
+    page.locator("#pwd_reg").fill("motyl55") #password
+    page.locator("#pwd1").fill("motyl55") #password2
+    page.locator("#name").fill("Jan")
+    page.locator("#surname").fill("Novák")
+    page.locator("#phone").fill("+420608888555")
+    page.locator("#street").fill("Horní 666")
+    page.locator("#city").fill("České Budějovice")
+    page.locator("#zip").fill("74755")
+    page.locator("#content > main > form > div.buttons > input").click()
+    #submit button
     assert page.url == "https://www.dumhudby.cz/registration.php?action=reg_ok"
 
